e7067b1082863135f81c3ef7ce4c1fa7b0fb400a
Dipankar <dipankar62918@gmail.com>
2021-02-02 20:21:53 +0530

    synchronization problem solved, server and client can send and recieve msg simultaneously

--- chat server/chatClient.c
+++ chat server/chatClient.c
@@ -25,40 +25,43 @@ void error(char *msg);
 
 int main(int argc, char const *argv[])
 {
-    //#1 Create socket
+    // Create socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1)
     {
         error("Failed to create socket");
     }
 
-    //set port and IP of server
+    // set port and IP of server
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, IP, &server_addr.sin_addr);
 
-    //#2 Send connection request to server:
-    if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+    // Send connection request to server:
+    if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+    {
         error("Unable to connect to server");
     }
     printf("Connected with server @IP: %s and port: %i\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
     printf("Enter 'END' to stop sending.\n");
 
-    //#3 Create thread for sending and recieving messages
+    // Create thread for sending and recieving messages
     pthread_t recvThread, sendThread;
-    if (pthread_create(&recvThread, NULL, recvMessage, NULL) != 0){
+    if (pthread_create(&recvThread, NULL, recvMessage, NULL) != 0)
+    {
         error("Thread creation failed");
     }
-    if (pthread_create(&sendThread, NULL, sendMessage, NULL) != 0){
+    if (pthread_create(&sendThread, NULL, sendMessage, NULL) != 0)
+    {
         error("Thread creation failed");
     }
 
-    //#4 wait for all the threads
+    // wait for all the threads
     pthread_join(recvThread, NULL);  
     pthread_join(sendThread, NULL);
 
-    //#5 Close socket
+    // Close socket
     close(client_socket);
     printf("All done!\n");
     return 0;
@@ -75,7 +78,8 @@ void *sendMessage(void *arg)
     while (strncmp(sendBuf, "END", 3))
     {
         fgets(sendBuf, BUFFER_SIZE, stdin);
-        if (send(client_socket, sendBuf, strlen(sendBuf), 0) < 0){
+        if (send(client_socket, sendBuf, strlen(sendBuf), 0) < 0)
+        {
             error("Couldn't send message");
         }
     }
@@ -88,8 +92,9 @@ void *recvMessage(void *arg)
     int msglen = 0;
     while (strncmp(sendBuf, "END", 3))
     {
-        msglen = recv(client_socket, recvBuf, sizeof(recvBuf), 0);
-        if (msglen < 0){
+        msglen = recv(client_socket, recvBuf, BUFFER_SIZE, 0);
+        if (msglen < 0)
+        {
             error("Couldn't receive message");
         } 
         if (strncmp(recvBuf, "END", 3) == 0)
